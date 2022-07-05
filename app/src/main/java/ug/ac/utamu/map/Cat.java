86935f520283c447e53206eae09599374adf275a
hkaluuma <hkaluuma@idi.co.ug>
2022-07-05 18:35:32 +0300

    inital Commit

---
+++ app/src/main/java/ug/ac/utamu/map/Cat.java
@@ -0,0 +1,12 @@
+package ug.ac.utamu.map;
+
+public class Cat {
+    //our variables below
+    private String Cat_name;
+    public String Habitat;
+    int weight;
+    String pyshicalAppearance;
+    /* Multiline comment */
+
+
+}
---
+++ app/src/main/java/ug/ac/utamu/map/Dog.java
@@ -0,0 +1,12 @@
+package ug.ac.utamu.map;
+
+public class Dog {
+    String Dog_Sound;
+    String dogMoves ="Dog move by ";
+    int Dog_walk;
+
+    public void dogWalk(){
+        String movelegs = "step by step .....";
+        String walking = dogMoves + movelegs;
+    }
+}
